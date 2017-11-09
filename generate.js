12bbb5c7516e50b634d6c2630de5f701d55cd7a8
Vladyslav <xinonghost@gmail.com>
2017-11-09 18:51:49 +0200

    Add forming transaction section for block.

--- generate.js
+++ generate.js
@@ -2,6 +2,8 @@
 
 var Transaction = require('./src/Transaction.js');
 var Block = require('./src/Block.js');
+var Formatter = require('./src/Formatter.js');
+var Blockchain = require('./src/Blockchain');
 
 var transactions = Transaction.getAllUnconfirmed();
 
@@ -10,6 +12,14 @@ if (transactions.length == 0) {
 	console.log('Empty block generation');
 } else {
 	transactions.map(function(e) {
-		//
+		var size = e.length;
+		txs += Formatter.formatHex(size, 8) + e;
 	});
+
+	var size = txs.length;
+	txs = Formatter.formatHex(size, 16) + txs;
 }
+
+var lastBlock = Blockchain.getLatest();
+
+var block = new Block();
